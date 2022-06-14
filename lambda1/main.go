872f6366656f0bc2d6941b007d41cad6758d0c5b
RayLai <raycblai@gmail.com>
2022-06-14 12:33:28 +0000

    lambda 2

--- lambda1/main.go
+++ lambda1/main.go
@@ -3,12 +3,21 @@ package main
 import (
     "fmt"
     "github.com/aws/aws-lambda-go/lambda"
+    //"github.com/aws/aws-lambda-go/events"
     )
     
 func main() {
     lambda.Start(Handler)
 }
 
-func Handler() {
-    fmt.Println("Function Invoked !!!")
+func Handler(event InputEvent) (string, error){
+    // fmt.Println("Function Invoked !!!")
+    fmt.Println(event.firstName)
+    fmt.Println(event.lastName)
+    return "It works !!",nil
+}
+
+type InputEvent struct {
+    firstName string `json:"firstname"`
+    lastName string `json:"lastname"`
 }
