e4e87c75891c8e0d444a64a0d046e441503c24a2
RayLai <raycblai@gmail.com>
2022-06-14 12:07:21 +0000

    add lambda1

---
+++ lambda1/go.mod
@@ -0,0 +1,5 @@
+module example/lambda1
+
+go 1.16
+
+require github.com/aws/aws-lambda-go v1.32.0 // indirect
---
+++ lambda1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "github.com/aws/aws-lambda-go/lambda"
+    )
+    
+func main() {
+    lambda.Start(Handler)
+}
+
+func Handler() {
+    fmt.Println("Function Invoked !!!")
+}
